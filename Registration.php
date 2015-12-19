a4933bba1666f1615a4911f27a1e67d29cc374ea
Nour Galaby <nour_traucer@hotmail.com>
2015-12-19 04:53:12 +0200

    start

---
+++ Registration.php
@@ -0,0 +1,62 @@
+<?php
+
+$Firstname = $_POST['fname'];
+$Lastname = $_POST['lname'];
+$Password = $_POST['pw'];
+$C_Password = $_POST['C_pw'];
+$EmailAdress = $_POST['Email'];
+$PhoneNumber = $_POST['pnumber'];
+$Birthdate_Day=$_POST['day'];
+$Birthdate_Month=$_POST['month'];
+$Birthdate_Year=$_POST['birthyear'];
+$Gender=$_POST['gender'];
+$Hometown=$_POST['hometown'];
+$MaritalStatus=$_POST['ms'];
+$AboutYou=$_POST['About'];
+
+
+
+$servername = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name="facebook";
+// Create connection
+
+
+
+$conn = new mysqli($servername, $db_username, $db_password,$db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: <br/> " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM member WHERE email='$EmailAdress'";
+
+if($result = mysqli_query($conn, $sql)){
+    if(mysqli_num_rows($result) > 0){
+	echo "Error: Username already exists ";
+ $cssFile = "Course.css";
+     echo "<link rel='stylesheet' href='" . $cssFile . "'>";
+     echo <<<HTML
+<body>
+
+</body>
+HTML;
+}
+else{
+$sql="INSERT INTO `member`( `email`, `password`, `first_name` , `last_name`, `gender` , `birthdate` , `phone_number` , `hometown` , `about_me` , `marital_status` )
+VALUES ('$EmailAdress' ,  md5('$Password') ,  '$Firstname' , '$Lastname' , '$Gender' , '$Birthdate_Year-$Birthdate_Month-$Birthdate_Day', '$PhoneNumber' , '$Hometown' , '$MaritalStatus' , '$AboutYou')";
+
+}
+}
+if($query=mysqli_query($conn,$sql))
+ {
+ /// Go to profile
+ die();
+  }
+   else
+   echo"error <br/>" .$sql. "<br>" .mysqli_error($conn);
+
+
+?>
