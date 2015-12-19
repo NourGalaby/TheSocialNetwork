b0670cd995f3a8333b1ca7d9a63ca3b3ba63b3d8
Nour Galaby <nour_traucer@hotmail.com>
2015-12-19 17:11:10 +0200

    connect fix

---
+++ connect.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+$servername = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_name="facebook";
+// Create connection
+
+
+
+$conn = new mysqli($servername, $db_username, $db_password,$db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: <br/> " . $conn->connect_error);
+} 
+
+
+ ?>
