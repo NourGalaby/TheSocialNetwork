4ef26a6d6a125a92c4f2dab72e7b1eb2949c7067
Nour Galaby <NourGalaby@users.noreply.github.com>
2015-12-24 00:36:03 +0200

    Update Login.php
    update login

--- Login.php
+++ Login.php
@@ -4,18 +4,7 @@ $Email = $_POST['email'];
 $Password = $_POST['pw'];
 
 
-$servername = "localhost";
-$db_username = "root";
-$db_password = "";
-$db_name="facebook";
-
-
-$conn = new mysqli($servername, $db_username, $db_password,$db_name);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: <br/> " . $conn->connect_error);
-}
-
+include("connect.php");
 
 $sql = "SELECT * FROM member WHERE email='$Email'";
 
