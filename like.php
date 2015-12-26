352ab0cf21a72854f964b56b38e1e6d87868a3a4
Nour Galaby <nour_traucer@hotmail.com>
2015-12-26 20:14:56 +0200

    added like redirect + Like Database

--- like.php
+++ like.php
@@ -6,4 +6,7 @@ $liked_post=$_POST['post_id'];
  
 $query=mysqli_query($conn,"INSERT INTO post_like (`member_id`,`post_id`) VALUES ('$mem_id','$liked_post')") or die(mysql_error());
 
+
+echo "<br>  <br> You will be redirirected to the main page in 1 secounds  <br> ";
+echo ' <meta http-equiv="refresh" content="0;url=profile.php" />';
 ?>
